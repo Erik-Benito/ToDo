127119f2a1c23e9e8983b7642f3e5a36e7ccff92
Erik Benito <Erik.aBenito@senacsp.edu.com>
2022-07-01 23:12:36 -0300

    dd messages home

---
+++ website/src/pages/message/chat/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+   display: flex;
+   flex-direction: column;
+
+   width: 100%;
+
+   .head{
+      display: flex;
+      flex-direction: row;
+
+      width: 100%;
+
+     img{
+
+      
+      border-radius: 50%;
+      max-height: 150px;
+
+     }
+
+   }
+
+`
+
+export { Container };
